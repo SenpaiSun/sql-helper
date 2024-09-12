ceb524c768b764feaed6b59da9ca4d6678d72d8a
SenpaiSun <www.maxxi@mail.ru>
2024-09-12 18:59:09 +0600

    feat: add styles for buttons

--- src/pages/InHelper/InHelper.tsx
+++ src/pages/InHelper/InHelper.tsx
@@ -2,24 +2,49 @@ import { Button } from '@shared/Button/Button'
 import { Flex, Button as ButtonAntd, Typography } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
 import { Content } from 'antd/es/layout/layout'
+import { useEffect, useState } from 'react'
 
 export const InHelper: React.FC = () => {
+
+  const [inputText, setInputText] = useState<string>('')
+  const [outputText, setOutputText] = useState<string>('')
+  // format 1 - преобразовывает в формат для MySQL (пример: id1, id2, id3)
+  // format 2 - преобразовывает в формат для Clickhouse (пример: 'id1', 'id2', 'id3')
+  const [selectedButton, setSelectedButton] = useState<number | null>(null)
+
+  // set default value
+  useEffect(() => {
+    const formatLocal = Number(localStorage.getItem('format'))
+    if (formatLocal) {
+      setSelectedButton(formatLocal)
+    } else {
+      setSelectedButton(1)
+    }
+  }, [])
+
+  // change format - id, or 'id',
+  const changeFormat = (num: number) => {
+    setSelectedButton(num)
+    localStorage.setItem('format', num.toString())
+  }
+  console.log(inputText)
+
   return (
     <Content className='w-full flex items-center justify-center' style={{ minHeight: '78vh' }}>
       <Flex className='flex-col' gap='small'>
         <Flex style={{ width: '40vw' }} className='justify-start' gap='large'>
           <Typography.Text className='text-white text-xl'>Format:</Typography.Text>
-          <ButtonAntd type='primary' className='text-white bg-[#3e423e] border-none hover:border-white focus:border-white w-20'>
+          <ButtonAntd type='primary' className={`text-white bg-[#3e423e] border-none hover:border-white focus:border-white w-20 ${selectedButton === 1 && 'bg-green-500'}`} style={{outline: 'none'}} onClick={() => changeFormat(1)}>
             id,
           </ButtonAntd>
-          <ButtonAntd type='primary' className='text-white bg-[#3e423e] border-none hover:border-white focus:border-white w-20'>
+          <ButtonAntd type='primary' className={`text-white bg-[#3e423e] border-none hover:border-white focus:border-white w-20 ${selectedButton === 2 && 'bg-green-500'}`} style={{outline: 'none'}} onClick={() => changeFormat(2)}>
             'id',
           </ButtonAntd>
         </Flex>
         <Flex gap='large'>
-          <TextArea style={{ minHeight: '40vh', width: '40vw', resize: 'none' }} className='bg-[#242424] hover:bg-[#242424] focus:bg-[#242424] border-white hover:border-white focus:border-white text-white'></TextArea>
+          <TextArea value={inputText} onChange={(e) => setInputText(e.target.value)} style={{ minHeight: '40vh', width: '40vw', resize: 'none' }} className='bg-[#242424] hover:bg-[#242424] focus:bg-[#242424] border-white hover:border-white focus:border-white text-white' />
           <Button />
-          <TextArea style={{ minHeight: '40vh', width: '40vw', resize: 'none', cursor: 'pointer' }} disabled></TextArea>
+          <TextArea value={outputText} style={{ minHeight: '40vh', width: '40vw', resize: 'none', cursor: 'pointer' }} disabled />
         </Flex>
       </Flex>
     </Content>
