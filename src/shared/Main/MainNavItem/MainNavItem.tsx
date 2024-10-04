5fa394488bea28b37e160841550f07ba35090859
SenpaiSun <www.maxxi@mail.ru>
2024-10-04 13:44:52 +0600

    feat: add color animation for span

--- src/shared/Main/MainNavItem/MainNavItem.tsx
+++ src/shared/Main/MainNavItem/MainNavItem.tsx
@@ -10,11 +10,14 @@ export const MainNavItem = ({ toPage, name }: PropsMainNavItem) => {
   return (
     <Link to={toPage} className='hover:opacity-80'>
       <Flex justify='center' align='center' gap={'small'}>
-        <span className='text-5xl bg-gradient-to-r from-yellow-300 via-blue-500 to-red-500 bg-size-300 text-transparent bg-clip-text animate-pulse'>&#187;&#187;&#187;</span>
+        {/* Левая стрелка с градиентом и анимацией */}
+        <span className='text-5xl bg-gradient-to-r from-pink-500 via-blue-500 to-green-500 bg-[length:300%_100%] text-transparent bg-clip-text animate-gradient-flow-left'>&#187;&#187;&#187;</span>
+        {/* Текст */}
         <Typography.Title level={2} style={{ color: 'white', margin: 0, marginTop: '0.5rem' }}>
           {name}
         </Typography.Title>
-        <span className='text-5xl bg-gradient-to-l from-red-500 via-blue-500 to-yellow-300 bg-size-300 text-transparent bg-clip-text animate-pulse'>&#171;&#171;&#171;</span>
+        {/* Правая стрелка с зеркальным градиентом и анимацией */}
+        <span className='text-5xl bg-gradient-to-r from-green-500 via-blue-500 to-pink-500 bg-[length:300%_100%] text-transparent bg-clip-text animate-gradient-flow-right'>&#171;&#171;&#171;</span>
       </Flex>
     </Link>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{html,js,tsx,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'gradient-flow-left': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        },
+        'gradient-flow-right': {
+          '0%': { 'background-position': '100% 50%' },
+          '50%': { 'background-position': '0% 50%' },
+          '100%': { 'background-position': '100% 50%' },
+        },
+      },
+      animation: {
+        'gradient-flow-left': 'gradient-flow-left 1.5s ease infinite',
+        'gradient-flow-right': 'gradient-flow-right 1.5s ease infinite',
+      },
+    },
   },
   plugins: [],
 }
