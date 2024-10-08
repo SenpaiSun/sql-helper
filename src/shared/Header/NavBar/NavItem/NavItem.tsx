48115e71e6fa5ea59639c7ce6864f1ed0884fe35
SenpaiSun <www.maxxi@mail.ru>
2024-10-08 19:11:30 +0600

    feat: add underline for header navbar

--- src/shared/Header/NavBar/NavItem/NavItem.tsx
+++ src/shared/Header/NavBar/NavItem/NavItem.tsx
@@ -1,4 +1,5 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import {clsx} from 'clsx';
 
 export interface PropsLink {
   toPage: string
@@ -6,7 +7,9 @@ export interface PropsLink {
 }
 
 export const NavItem = ({toPage, name}: PropsLink) => {
+  const currentRoute = useLocation()
+  console.log(currentRoute.pathname, toPage)
   return (
-    <RouterLink className='text-white hover:text-gray-300' to={toPage}>{name}</RouterLink>
+    <RouterLink className={`text-white hover:text-gray-300 ${clsx(currentRoute.pathname === toPage && 'underline hover:underline focus:underline text-gray-400')}`} to={toPage}>{name}</RouterLink>
   );
 };
