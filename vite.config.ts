60fbb9a50b29a794e01791451043bec4e11bb4f7
SenpaiSun <www.maxxi@mail.ru>
2024-11-21 19:05:22 +0600

    fix: create dependencies for build dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', 
+    },
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/App'),
