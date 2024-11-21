8928815b2f8fd990e5482e27e3f185d9115d09a7
SenpaiSun <www.maxxi@mail.ru>
2024-11-21 19:54:24 +0600

    feat: add nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/sql-helper/',
+  base: '/sql-helper',
   build: {
     outDir: 'dist',
     rollupOptions: {
